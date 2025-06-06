cdd1cea859122c8cf0dc3edcd117e108a91e134e
ShiowinNobela <yamabukijod@gmail.com>
2025-06-07 01:32:34 +0800

    updates

---
+++ src/components/WalkInPopUp.jsx
@@ -0,0 +1,15 @@
+
+function WalkInPopUp({open, onClose, children}) {
+  return (
+    <>
+    <div onClick={onClose} className={open? " fixed flex items-center justify-center inset-0 transition-colors bg-black/20 " : "hidden"}  >
+        <div onClick={e => e.stopPropagation()} className={open? "bg-white rounded-xl shadow p-6 transition-all scale-100 opacity-100" : "scale-125 opacity-0"} >
+            <button onClick={onClose} className="absolute top-2 right-2 p-1 rounded-lg text-gray-400 bg-white hover:bg-gray-50 hover:text-gray-600" > X </button>
+            {children}
+        </div> 
+    </div>
+    </>
+  )
+}
+
+export default WalkInPopUp
