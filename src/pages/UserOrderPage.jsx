97b207fc6dba71f7416f37c6bf34eff3d806c20f
ShiowinNobela <yamabukijod@gmail.com>
2025-06-08 23:18:18 +0800

    adjust

--- src/pages/UserOrderPage.jsx
+++ src/pages/UserOrderPage.jsx
@@ -160,7 +160,7 @@ function UserViewOrderPage() {
   
 
   return (
-    <section className="bg-amber-50 min-h-full pt-20 ">
+    <section className="bg-amber-50 min-h-full ">
       <div className="grid grid-cols-[0.15fr_0.85fr]">
         <UserSideBar setActiveSwitch={setActiveSwitch} />
         <div>
@@ -331,43 +331,46 @@ function UserViewOrderPage() {
           )}
 
           {activeSwitch === "notif" && (
-            <div className="flex items-center justify-center min-h-[60vh] w-full">
-              <div className="w-4xl bg-gray-50 h-50 mx-auto shadow-md drop-shadow-xl">
-                <caption className="text-xl font-bold p-5">
+            <div className="flex items-center justify-center min-h-[60vh] w-full mt-10">
+              <div>
+               <caption className="text-xl font-bold p-5">
                   Notifications
                 </caption>
-                <div className="flex flex-col items-center justify-center">
-                  {notifications.length === 0 ? (
-                    <div className="text-gray-500 p-5">
-                      No notifications yet.
-                    </div>
-                  ) : (
-                    notifications.map((notif) => (
-                      <div
-                        key={notif.id}
-                        className="w-3xl grid grid-cols-[0.70fr_0.30fr] bg-white shadow-2xl mb-4"
-                      >
-                        <div className="flex flex-col p-5 pl-10 justify-start">
-                          <h1 className="text-lg font-semi text-start ">
-                            {notif.message}
-                          </h1>
-                          <p className="text-sm">
-                            Delivered on:{" "}
-                            {new Date(notif.date).toLocaleString()}
-                          </p>
-                        </div>
-                        <div
-                          className="flex flex-col p-5 pr-10 justify-end items-end"
-                          onClick={() => {
-                            window.location.href = `/GiveReview/${notif.id} `;
-                          }}
-                        >
-                          <p>Leave a Review!</p>
-                          <MdOutlineRateReview className="h-15 w-15 " />
-                        </div>
+                <div className="w-4xl bg-gray-50 overflow-y-auto h-150 mx-auto shadow-md drop-shadow-xl">
+                
+                  <div className="flex flex-col items-center justify-center">
+                    {notifications.length === 0 ? (
+                      <div className="text-gray-500 p-5">
+                        No notifications yet.
                       </div>
-                    ))
-                  )}
+                    ) : (
+                      notifications.map((notif) => (
+                        <div
+                          key={notif.id}
+                          className="w-3xl grid grid-cols-[0.70fr_0.30fr] bg-white shadow-2xl mb-4"
+                        >
+                          <div className="flex flex-col p-5 pl-10 justify-start">
+                            <h1 className="text-lg font-semi text-start ">
+                              {notif.message}
+                            </h1>
+                            <p className="text-sm">
+                              Delivered on:{" "}
+                              {new Date(notif.date).toLocaleString()}
+                            </p>
+                          </div>
+                          <div
+                            className="flex flex-col p-5 pr-10 justify-end items-end"
+                            onClick={() => {
+                              window.location.href = `/GiveReview/${notif.id} `;
+                            }}
+                          >
+                            <p>Leave a Review!</p>
+                            <MdOutlineRateReview className="h-15 w-15 " />
+                          </div>
+                        </div>
+                      ))
+                    )}
+                  </div>
                 </div>
               </div>
             </div>
