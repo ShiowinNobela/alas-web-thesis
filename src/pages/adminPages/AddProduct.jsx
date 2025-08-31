58cd48d51915dab1f1915e964229af70fb0e5fc0
Arrow <delmontearrow2004@gmai;.com>
2025-08-31 10:25:14 +0800

    Fix for deployment

--- src/pages/adminPages/AddProduct.jsx
+++ src/pages/adminPages/AddProduct.jsx
@@ -2,11 +2,7 @@ import { useState } from 'react';
 import axios from 'axios';
 import { Toaster, toast } from 'sonner';
 import { useNavigate } from 'react-router-dom';
-import BaseInput from '../../components/Chinges/BaseInput.jsx';
-import Description from '../../components/Chinges/Description.jsx';
-import DropDown from '../../components/Chinges/DropDown.jsx';
-import Upload from '../../components/bigComponents/Upload.jsx';
-import BackButton from '@/components/bigComponents/BackButton.jsx';
+
 import { Button } from 'flowbite-react';
 
 function AddProduct() {
@@ -45,77 +41,15 @@ function AddProduct() {
     <>
       <div className="flex h-full flex-col overflow-x-auto bg-white">
         <div className="bg-gray-50">
-          <div className="flex w-full items-center justify-between pt-5">
-            <BackButton onClick={() => navigate('/Admin/ProductManagement')} />
-          </div>
+          <div className="flex w-full items-center justify-between pt-5"></div>
           <div className="grid h-135 w-full grid-cols-2 gap-7 px-8 pt-5">
             <div className="flex h-full flex-col gap-3 rounded-2xl bg-gray-800 p-7">
-              <BaseInput
-                label="Product Id"
-                className="pb-5"
-                onChange={(e) => setValues({ ...values, id: e.target.value })}
-              />
-              <BaseInput
-                label="Product Name "
-                onChange={(e) => setValues({ ...values, name: e.target.value })}
-              />
-              <Description
-                label="Product Description"
-                onChange={(e) =>
-                  setValues({ ...values, description: e.target.value })
-                }
-              />
-              <div className="grid grid-cols-[0.6fr_0.4fr] gap-4">
-                <BaseInput
-                  label="Quantity"
-                  onChange={(e) =>
-                    setValues({ ...values, stock_quantity: e.target.value })
-                  }
-                />
-                <DropDown
-                  label="Category"
-                  options={[
-                    { value: '', label: '-- select an option --' },
-                    { value: 'hot_sauce', label: 'Hot Sauce' },
-                    { value: 'chili_oils', label: 'Chili Oils' },
-                    { value: 'pickled_jalopeno', label: 'Pickled Jalopeno' },
-                    { value: 'limited_item', label: 'Limited Item' },
-                  ]}
-                  value={values.category}
-                  onChange={(e) =>
-                    setValues({ ...values, category: e.target.value })
-                  }
-                />
-              </div>
+              <div className="grid grid-cols-[0.6fr_0.4fr] gap-4"></div>
             </div>
             <div className="h-full rounded-2xl bg-gray-800">
-              <div className="px-7 pt-8">
-                <Upload
-                  onUploadSuccess={(url) =>
-                    setValues({ ...values, image: url })
-                  }
-                />
-              </div>
+              <div className="px-7 pt-8"></div>
               <div className="flex flex-col gap-3 bg-gray-800 p-7">
-                <DropDown
-                  label="Spice Level (Future Addition)"
-                  options={[
-                    { value: 'Mild', label: 'Mild' },
-                    { value: 'Spicy', label: 'Spicy' },
-                    { value: 'Very Spicy', label: 'Very Spicy' },
-                    { value: 'Extreme', label: 'Extreme' },
-                  ]}
-                />
                 <div className="flex w-full justify-between">
-                  <BaseInput
-                    label="Price"
-                    onlyNumber
-                    min="0"
-                    value={values.price}
-                    onChange={(e) =>
-                      setValues({ ...values, price: e.target.value })
-                    }
-                  />
                   <div className="mt-8">
                     <Button color="gray" onClick={handleSubmit}>
                       Upload
