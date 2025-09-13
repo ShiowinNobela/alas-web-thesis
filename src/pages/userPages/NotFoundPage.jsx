d44462fdf75415b24d91fd46440cb188291dd8af
Your Name <your_email@example.com>
2025-09-13 08:14:35 +0800

    404

--- src/pages/userPages/NotFoundPage.jsx
+++ src/pages/userPages/NotFoundPage.jsx
@@ -1,16 +1,15 @@
 import { Button } from '@/components/ui/button';
 import { Link } from 'react-router-dom';
-import { AlertTriangle, CircleQuestionMark } from 'lucide-react';
+  import { AlertTriangle } from 'lucide-react';
 
 function NotFoundPage() {
   return (
-    <section className="bg-neutral min-h-screen p-8">
-      <CircleQuestionMark className="text-primary size-10" />
-      <div className="flex items-center gap-2">
-        <AlertTriangle className="text-primary" />
-        <h1 className="text-2xl font-bold">404 - Page Not Found</h1>
+    <section className="bg-neutral min-h-screen flex flex-col items-center justify-center px-6 text-center">
+      <div className="flex items-center justify-center gap-2 mb-2 ">
+        <AlertTriangle className="text-red-500 size-30" />
+        <h2 className="text-3xl font-bold">404 - Page Not Found</h2>
       </div>
-      <p className="mt-2 text-gray-600">
+      <p className="mt-2 max-w-xl text-gray-600">
         The page you are looking for doesn’t exist, may have been moved, or the
         link you followed might be broken. Please check the URL or return to the
         homepage.
