3d1078bf840a144ffeeefc73bd0d8a93793911cd
ArrowMupas <delmontearrow2004@gmail.com>
2025-10-22 22:27:28 +0800

    I dont wanna do this anymore

--- src/pages/home/LandingPage.jsx
+++ src/pages/home/LandingPage.jsx
@@ -3,8 +3,21 @@ import StorySection from './sections/StorySection';
 import HeroSection from './sections/HeroSection';
 import TestimonialSection from './sections/TestimonialSection';
 import HotDealSection from './sections/HotDealSection';
+import { useEffect } from 'react';
+import axios from 'axios';
 
 const LandingPage = () => {
+  useEffect(() => {
+    axios
+      .get('/api/products')
+      .then(() => {
+        console.log('✅ Backend warmed up successfully');
+      })
+      .catch(() => {
+        // silently ignore any errors
+      });
+  }, []);
+
   return (
     <>
       <HeroSection />
